ca331aee92b47776594861f82061ae88d6c1951f
Rahul Kumar Parihar <rahulkumarpariharmailbox@gmail.com>
2022-09-07 19:23:00 +0530

    Adding Merge sort implementation in Arrays

--- Recursion/Arrays.java
+++ Recursion/Arrays.java
@@ -1,17 +1,25 @@
-public class Arrays<T> {
-    T[] array;
+public class Arrays {
+    int[] array;
 
-    public Arrays(T[] array) {
+    public Arrays(int[] array) {
         this.array = array;
     }
 
     public void print() {
-        printRecursive(array, 0);
+        printRecursive(array, 0, array.length);
     }
 
-    private void printRecursive(T[] arr, int index) {
+    public void reverse() {
+        reverseRecursive(array, 0, array.length - 1);
+    }
+
+    public void mergeSort() {
+        mergeSortHelper(array, 0, array.length - 1);
+    }
+
+    private void printRecursive(int[] arr, int index, int end) {
         // base case
-        if (index == arr.length) {
+        if (index == end) {
             System.out.println();
             return;
         }
@@ -20,32 +28,90 @@ public class Arrays<T> {
         System.out.print(arr[index] + " ");
 
         // Recursive call
-        printRecursive(arr, index + 1);
+        printRecursive(arr, index + 1, end);
     }
 
-    public void reverse() {
-        reverseRecursive(array, 0, array.length - 1);
-    }
-
-    private void reverseRecursive(T[] arr, int start, int end) {
+    private void reverseRecursive(int[] arr, int start, int end) {
         // base class
-        if(start > end) {
+        if (start > end) {
             return;
         }
 
         // process
-        T temp = arr[start];
+        int temp = arr[start];
         arr[start] = arr[end];
         arr[end] = temp;
-        
+
         // recursive call
         reverseRecursive(arr, start + 1, end - 1);
     }
 
+    private void mergeSortHelper(int[] arr, int start, int end) {
+        // base case
+        if (start >= end) {
+            return;
+        }
+
+        int mid = (end - start) / 2 + start;
+
+        // left part
+        mergeSortHelper(arr, start, mid);
+        // right part
+        mergeSortHelper(arr, mid + 1, end);
+
+        // merge
+        mergeArray(arr, start, mid, end);
+    }
+
+    private void mergeArray(int[] arr, int start, int mid, int end) {
+        int len1 = (mid - start) + 1;
+        int len2 = (end - mid - 1) + 1;
+
+        int mainArrayIndex = start;
+
+        int[] array1 = new int[len1];
+        int[] array2 = new int[len2];
+
+        for (int i = start, j = 0; i <= mid; i++, j++) {
+            array1[j] = arr[i];
+        }
+
+        for (int i = mid + 1, j = 0; i <= end; i++, j++) {
+            array2[j] = arr[i];
+        }
+
+        int index1 = 0;
+        int index2 = 0;
+
+        while (index1 < len1 && index2 < len2) {
+            if (array1[index1] < array2[index2]) {
+                arr[mainArrayIndex] = array1[index1];
+                mainArrayIndex++;
+                index1++;
+            } else {
+                arr[mainArrayIndex] = array2[index2];
+                mainArrayIndex++;
+                index2++;
+            }
+        }
+
+        while (index1 < len1) {
+            arr[mainArrayIndex] = array1[index1];
+            mainArrayIndex++;
+            index1++;
+        }
+
+        while (index2 < len2) {
+            arr[mainArrayIndex] = array2[index2];
+            mainArrayIndex++;
+            index2++;
+        }
+    }
+    
     public static void main(String args[]) {
-        Arrays<Integer> arrays = new Arrays<>(new Integer[] { 1, 2, 3, 4, 5 });
+        Arrays arrays = new Arrays(new int[] { 12, 3, 99, 99, 38, 27, 43, 3, 9, -1, 12, 82 });
         arrays.print();
-        arrays.reverse();
+        arrays.mergeSort();
         arrays.print();
     }
 }
